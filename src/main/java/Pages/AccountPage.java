b9d62fdc082798268019fae977359d8b4288eada
mohanakrishna_v <mohanakrishna.v@fridaymediagroup.com>
2022-04-21 00:08:41 +0530

    dev1 second commit

--- src/main/java/Pages/AccountPage.java
+++ src/main/java/Pages/AccountPage.java
@@ -2,5 +2,6 @@ package Pages;
 
 public class AccountPage {
 
-    //This is a new comment from Dev1
+    //This is a new comment from Dev1 and also this is the update
+    //this is a new second comment
 }
