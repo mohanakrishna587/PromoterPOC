9e9896c0d3abf3beef1cc3953162f84494c92a63
sathishn001 <34599852+sathishn001@users.noreply.github.com>
2022-04-21 12:46:00 +0530

    Hi there

--- src/main/java/Pages/AccountPage.java
+++ src/main/java/Pages/AccountPage.java
@@ -4,4 +4,12 @@ public class AccountPage {
 
     //This is a new comment from Dev1 and also this is the update
     //this is a new second comment
+
+    public static void main(String[] args) {
+        //testing on commit
+    }
+
+    public void sample(){
+        
+    }
 }
